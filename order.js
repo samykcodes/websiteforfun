c72ef77eaddd0356ee4af66a480602c0d9d74eaf
SENONC <82212785+SENONC@users.noreply.github.com>
2021-10-30 16:46:00 +0100

    Add files via upload

---
+++ order.js
@@ -0,0 +1,6 @@
+function check(){
+    var box = document.getElementById("secret").value;
+    if(box == "Secret"){
+        alert("The secret code is correct! You will be redirected now...")
+    }
+}
